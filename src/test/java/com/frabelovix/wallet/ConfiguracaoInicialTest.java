72440be8821ce2fd8f8aa1e5a6903558d5d9178e
frabelovix <frabelovix@gmail.com>
2021-10-30 09:23:05 -0300

    Configuração inicial para os testes

--- src/test/java/com/frabelovix/wallet/ConfiguracaoInicialTest.java
+++ src/test/java/com/frabelovix/wallet/ConfiguracaoInicialTest.java
@@ -1,15 +1,18 @@
 package com.frabelovix.wallet;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class WalletApplicationTests {
-
+public class ConfiguracaoInicialTest {
+	
 	@Test
-	void contextLoads() {
+	public void testDeveFuncionarConfiguracaoTestes(){
+	 assertEquals(1,1);
 	}
 
 }
